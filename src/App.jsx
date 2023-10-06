d8e378a973d17f3f3e99b61de0d5572ff9f4ab8a
Hao Vo <haovo1512@gmail.com>
2023-10-06 21:57:23 +0700

    ++upcode

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Path from './utils/path'
 import Public from './pages/Public'
 import Home from './pages/Home'
 import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path={Path.PUBLIC} element={<Public />}>
           <Route path={Path.HOME} element={<Home />} />
           <Route path={Path.LOGIN} element={<Login />} />
+          <Route path={Path.REGISTER} element={<Register />} />
         </Route>
       </Routes>
     </div>
