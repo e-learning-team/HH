0bb866d85e37243376c911de7960ddd8228d7c17
thomsonbel12 <thomsonbel12@gmail.com>
2023-10-13 22:03:21 +0700

    ++upcode bug ??

--- src/apis/axios.jsx
+++ src/apis/axios.jsx
@@ -3,6 +3,7 @@ import { apiRefreshToken } from './refreshToken';
 import { useDispatch, useSelector } from "react-redux";
 import { logout, updateToken } from '../store/User/userSlice';
 import { store } from '../store/configureStore';
+import { apiLogOut } from './user';
 const instance = axios.create({
     baseURL: 'http://localhost:8080/e-learning/api',
     'Content-Type': 'application/json',
@@ -25,23 +26,26 @@ instance.interceptors.response.use(function (response) {
 
     return response?.data;
 }, async function (error) {
-
     console.log("response status code---" + error.response.status);
     console.log("response config---" + error.config);
     const prevRequest = error?.config;
+
     if (error.response && error?.response?.status === 401 && !prevRequest?.sent) {
         prevRequest.sent = true;
         const resp =  await apiRefreshToken();
         store.dispatch(updateToken(resp?.data?.token));
         return instance(prevRequest);
-            // .then(res => {
-            //     store.dispatch(updateToken(res?.data?.token));
-            //     // prevRequest.headers['authorization'] = 'Bearer ' + res?.data?.token;
-            //     return instance(prevRequest);
-            // });
+    }
+    try{
+        await apiLogOut();
+        store.dispatch(logout())
+        prevRequest.headers['authorization'] = null
+        return instance(prevRequest);
+    }catch(err){
+        return Promise.reject(error);
+
     }
 
-    return Promise.reject(error);
 });
 
 export default instance;
