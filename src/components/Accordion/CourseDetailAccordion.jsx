35c8d6877f85dcde892fcac269fb3147713462ff
thomsonbel12 <thomsonbel12@gmail.com>
2023-10-27 20:12:14 +0700

    ++upcode new branch ?

---
+++ src/components/Accordion/CourseDetailAccordion.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
+} from "@material-tailwind/react";
+import { faFileVideo } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function Icon({ open }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className={`${open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+    );
+}
+export default function CustomAccordion({ title, content }) {
+    const [alwaysOpen, setAlwaysOpen] = React.useState(false);
+
+    const handleAlwaysOpen = () => setAlwaysOpen((cur) => !cur);
+
+    return (
+
+        <div className="shadow-md">
+            {content.length > 0 && (
+                <div>
+                    <Accordion open={alwaysOpen} className="border border-gray-400 "
+                        icon={
+                            <span className="text-sm font-normal flex items-center gap-3">
+                                <span>{content.length} bài học</span>
+                                <span>•</span>
+                                {/* <span>15 phút</span> */}
+                            </span>
+                        }>
+                        <AccordionHeader
+                            onClick={() => handleAlwaysOpen()}
+                            className={`bg-zinc-100 border-b border-gray-300 px-4 transition-colors ${alwaysOpen ? "text-[#003a47]" : ""}`}>
+                            {
+                                <span className="flex items-center gap-4">
+                                    <Icon open={alwaysOpen} />
+                                    {title}
+                                </span>
+                            }
+                        </AccordionHeader>
+                        <AccordionBody className="px-4 text-base font-normal">
+                            {
+                                content.map((ct) => (
+                                    <div key={ct.id} className="text-sm my-2 flex items-center justify-between">
+                                        <div className="flex items-center gap-3">
+                                            <FontAwesomeIcon className="text-lg" icon={faFileVideo} />
+                                            <div>{ct.name}</div>
+                                        </div>
+                                        <div>
+                                            <span>15 phút</span>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </AccordionBody>
+                    </Accordion>
+                </div>
+            )}
+        </div>
+    );
+}
---
+++ src/pages/Dialog/VideoReviewDialog.jsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export function VideoReviewDialog({ videoTitle, videoPath, open, setOpen }) {
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const scroll = 'paper';
+    return (
+        <div>
+            <Dialog
+                className=''
+                open={open}
+                scroll={scroll}
+                aria-labelledby="scroll-dialog-title"
+                aria-describedby="scroll-dialog-description"
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}>
+                <DialogTitle className='min-w-[15rem] ' id='scroll-dialog-title ' >
+                    {
+                        <span className='flex items-center justify-between '>
+                            {'videoTitle'}
+                            <span onClick={handleClose} className='cursor-pointer'>
+                                <FontAwesomeIcon className='p-1' icon={faXmark} />
+                            </span>
+                        </span>
+                    }
+                </DialogTitle>
+                <DialogContent className=' border-b border-gray-400' dividers={scroll === 'paper'} >
+                    <DialogContentText className=' min-h-[34rem]' tabIndex={-1} id="scroll-dialog-description">
+                        <span className='relative h-0 pb-[56.25%] flex justify-center mb-6'>
+                            <iframe
+                                className='absolute top-0 left-0 w-full h-full'
+                                src="https://drive.google.com/file/d/1giM4L6MBKP2709OaVT1umBK-oHKPUAaL/preview"
+                                width="640"
+                                height="480"
+                                allow="autoplay">
+                            </iframe>
+                            {/* <video width="560" height="560" controls>
+                                <source src="https://drive.google.com/file/d/1giM4L6MBKP2709OaVT1umBK-oHKPUAaL/preview" type="video/mp4" />
+                                oops
+                            </video> */}
+                        </span>
+                        {[...new Array(50)]
+                            .map(
+                                () => `Cras mattis consectetur purus sit amet fermentum.
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                                Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                            )
+                            .join('\n')}
+                    </DialogContentText>
+                </DialogContent>
+                {/* <DialogActions>
+                    
+                </DialogActions> */}
+            </Dialog>
+        </div>
+    );
+}
